e4b3f3414df7b6be68533a806d8c60fdfacf12f6
moonquay <growyourownfood@gmail.com>
2020-08-10 19:22:22 -0600

    updated app.py

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
-from flask import Flask
 import os
-from dotenv import load_dotenv
-import quandl
-import yfinance
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-import plotly.offline as py
+from flask import Flask
+import plotly.graph_objects as go
+import plotly.express as px
+from utils import fetch_data, build_model
+import pandas as pd
 
-load_dotenv()
+if not os.path.exists('data'):
+    os.makedirs('data')
+if not os.path.exists('models'):
+    os.makedirs('models')
 
-quandl.ApiConfig.api_key = os.getenv('QUANDL_KEY')
-
-msft_obj = yfinance.Ticker('MSFT')
-msft_df = msft_obj.history(period='max')
-msft = msft_df.reset_index()[['Date', 'Close']].rename(columns={'Date': 'ds', 'Close': 'y'})
-
-m = Prophet()
-m.fit(msft)
-
+# Instantiate an app
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
+
 
 @app.route('/')
-def hello_world():
-    return 'YEEHAW'
+def home():
+    return 'Use the following endpoint: /data/tickername OR /plot/tickername'
 
-@app.route('/<tickername>')
-def predict(tickername):
-    future = m.make_future_dataframe(periods=30)
-    forecast = m.predict(future)
-    return forecast.to_dict()['yhat']
+
+@app.route('/data/<tickername>')
+def data(tickername):
+    data = fetch_data(tickername)
+    # data.to_csv(f'data/{tickername}.csv')
+    return data.reset_index().to_html()
+
+
+@app.route('/plot/<tickername>')
+def plot(tickername):
+    data = fetch_data(tickername).reset_index()
+    # data = pd.read_csv(f'data/{tickername}.csv')
+    fig = go.Figure([go.Scatter(x=data['Date'], y=data['Close'])])
+    return fig.to_html()
+
+
+@app.route('/forecast/<tickername>/<steps>')
+def forecast(tickername, steps):
+    model = build_model(tickername)
+    fcast = model.forecast(int(steps))
+    return str(fcast[0])
+
+
+@app.route('/forecast_plot/<tickername>/<steps>')
+def forecast_plot(tickername, steps):
+    data = fetch_data(tickername).reset_index()
+    data['Type'] = 'HISTORICAL'
+    model = build_model(tickername)
+    fcast = model.forecast(int(steps))
+    new_series = pd.date_range(data['Date'].iloc[-1], periods=int(steps))
+    fcast_df = pd.DataFrame({'Date': new_series,
+                             'Close': fcast[0],
+                             'Type': 'FORECAST'})
+    final_df = pd.concat([data[['Date', 'Close', 'Type']], fcast_df])
+    fig = px.line(final_df, x='Date', y='Close', color='Type')
+    # fig = go.Figure([go.Scatter(x=data['Date'], y=data['Close'])])
+    # fig.add_trace(go.Scatter(x=fcast['Date'], y=fcast[0]))
+    return fig.to_html()
+
+    # return 'you should have a plot of the data + forecast here'
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
